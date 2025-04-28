10a3a374eae7f179ba031d14a036a0b119b0a848
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-04-28 16:01:32 -0300

    Criação do Arquivo pre_historia
    Criei o componente Navegação e Body

---
+++ pre_historia/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Body from './Componentes/Body';
+import Navegacao from './Componentes/Navegacao';
+
+function App() {
+  return (
+    <div className="App">
+     <Navegacao />
+     <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ pre_historia/src/Componentes/Body.js
@@ -0,0 +1,16 @@
+import './Body.css';
+
+
+function Body() {
+    return (
+      <div className="Body">
+
+       <Body>
+            <h1>Olá Mundo</h1>
+       </Body>
+
+      </div>
+    );
+  }
+  
+  export default Body;
