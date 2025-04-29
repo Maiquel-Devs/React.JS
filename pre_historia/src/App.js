edddef9d19590f72ecab08952f9af9e3c26c4c3b
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-04-29 18:12:02 -0300

    Problema Resolvido

---
+++ pre_historia/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Body from './Componenetes/Body';
+import Idade_Pedra from './Componenetes/Idade_Pedra';
+import Nav from './Componenetes/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+
+      <Nav />
+      <Idade_Pedra />
+      
+    </div>
+  );
+}
+
+export default App;
